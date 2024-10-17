8727f742850152915a3fd09e1b2053258b315770
quispcchc <quisp@carlingtonchc.org>
2024-10-17 15:10:35 -0400

    update gitignore and database config for deployment

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 
+const db_config = {}
+
+if (process.env.NODE_ENV === 'development') {
+  db_config['host'] = process.env.DB_HOST;
+  db_config['port'] = process.env.DB_PORT;
+} else {
+  db_config.socketPath = `/cloudsql/${process.env.DB_INSTANCE}`;
+}
+
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'your-secret-key',
   EMAIL_USER: process.env.EMAIL_USER,
   EMAIL_PASS: process.env.EMAIL_PASS,
   DB_CONFIG: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'your-database-password',
-    database: process.env.DB_NAME || 'your-database-name'
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'your-database-name',
+    ...db_config
   }
 };
